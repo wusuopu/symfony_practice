2d49e6335f6e4237d02ae1507dd4303ccd7ef8ac
wusuopu <admin@longchangjin.cn>
2014-05-15 18:09:27 +0800

    finish chapter 02

--- src/Blogger/BlogBundle/Controller/PageController.php
+++ src/Blogger/BlogBundle/Controller/PageController.php
@@ -3,11 +3,40 @@
 namespace Blogger\BlogBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Blogger\BlogBundle\Entity\Enquiry;
+use Blogger\BlogBundle\Form\EnquiryType;
 
 class PageController extends Controller
 {
     public function aboutAction()
     {
         return $this->render('BloggerBlogBundle:Page:about.html.twig');
+    }
+
+    public function contactAction()
+    {
+        $enquiry = new Enquiry();
+        $form = $this->createForm(new EnquiryType(), $enquiry);
+
+        $request = $this->getRequest();
+        if ($request->getMethod() == 'POST') 
+        {
+          $form->bind($request);
+
+          if ($form->isValid()) {
+            // 
+            $message = \Swift_Message::newInstance()
+              ->setSubject('Contact enquiry from symblog')
+              ->setFrom('13554499384@163.com')
+              ->setTo('longchangjin@linuxdeepin.com')
+              ->setBody($this->renderView('BloggerBlogBundle:Page:contactEmail.txt.twig', array('enquiry' => $enquiry)));
+            $this->get('mailer')->send($message);
+
+            $this->get('session')->set('blogger-notice', 'Your contact enquiry was successfully sent. Thank you!');
+            return $this->redirect($this->generateUrl('blogger_blog_contact'));
+          }
+        }
+
+        return $this->render('BloggerBlogBundle:Page:contact.html.twig', ['form'=> $form->createView()]);
     }
 }
--- src/Blogger/BlogBundle/Form/EnquiryType.php
+++ src/Blogger/BlogBundle/Form/EnquiryType.php
@@ -1,20 +1,22 @@
 <?php
-
-namespace Acme\DemoBundle\Form;
+namespace Blogger\BlogBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 
-class ContactType extends AbstractType
+class EnquiryType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $builder->add('name');
         $builder->add('email', 'email');
-        $builder->add('message', 'textarea');
+        $builder->add('subject');
+        $builder->add('body', 'textarea');
     }
 
     public function getName()
     {
-        return 'contact';
+        return 'blogger_blogbundle_enquirytype';
     }
 }
+?>
